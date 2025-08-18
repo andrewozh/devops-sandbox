2e194551319d17aef4f59d14bb5566f54f64003f
andrewozh <andrew.ozhegov@gmail.com>
2025-08-18 06:20:24 +0300

    local git server fix
    fix shallow

--- demo/bootstrap.sh
+++ demo/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git clone --depth 1 https://github.com/andrewozh/devops-sandbox.git
+git clone --depth 2 https://github.com/andrewozh/devops-sandbox.git
 cd devops-sandbox
 make killercoda -C demo
