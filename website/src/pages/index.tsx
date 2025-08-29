d64ba737d8bd3f93bdfdc994a3b426cfc9aec2d5
andrewozh <andrew.ozhegov@gmail.com>
2025-08-29 09:55:08 +0300

    web: fix main text

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -78,8 +78,8 @@ function HomepageHeader() {
         <div className={styles.heroDescription}>
           <p className={styles.descriptionText}>
             My goal here, is to combine the most useful devops practices and tools into a single sophisticated project,
-            that can be used not only as a <span className={styles.accentWord}>demo</span> of my skills and <span className={styles.accentWord}>sandbox</span> for learning new tools and practices but also a
-            the infra <span className={styles.accentWord}>foundation</span> for the company of any size.
+            that can be used not only as a <span className={styles.accentWord}>demo</span> of my skills and <span className={styles.accentWord}>sandbox</span> for learning new tools but also as
+            infra <span className={styles.accentWord}>foundation</span> for companies of any size.
           </p>
         </div>
         <div className={styles.heroButtons}>
