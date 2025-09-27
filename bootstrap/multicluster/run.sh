bb10efd7e5400554d96a6f525d5826d51ce889dd
andrewozh <andrew.ozhegov@gmail.com>
2025-09-27 13:23:16 +0300

    final multicluster setup

--- bootstrap/multicluster/run.sh
+++ bootstrap/multicluster/run.sh
@@ -27,20 +27,28 @@ chmod +x gen-eastwest-gateway.sh
 # curl https://raw.githubusercontent.com/istio/istio/release-1.27/samples/multicluster/expose-istiod.yaml -O
 kubectl apply --context="${CTX_CLUSTER1}" -n istio-system -f expose-istiod.yaml
 
+kubectl --context="${CTX_CLUSTER1}" apply -n istio-system -f \
+  ./expose-services.yaml
+
 # -------
 
 kind create cluster --name cluster2
 
+kubectl create ns --context="${CTX_CLUSTER2}" metallb-system
+helm install metallb metallb/metallb --namespace metallb-system
+for t in {1..10}; do
+  kubectl apply --context="${CTX_CLUSTER2}" -f metallb-config.yaml && break || sleep $t
+done
+
 kubectl --context="${CTX_CLUSTER2}" create namespace istio-system
 kubectl --context="${CTX_CLUSTER2}" annotate namespace istio-system topology.istio.io/controlPlaneClusters=cluster1
+kubectl label --context="${CTX_CLUSTER2}" namespace istio-system topology.istio.io/network=network2
 
 export DISCOVERY_ADDRESS=$(kubectl \
   --context="${CTX_CLUSTER1}" \
   -n istio-system get svc istio-eastwestgateway \
   -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-
 yq '.spec.values.global.remotePilotAddress = "'${DISCOVERY_ADDRESS}'"' cluster2.yaml
-
 istioctl install --context="${CTX_CLUSTER2}" -f cluster2.yaml
 
 export HOSTIP_CLUSTER2=$(kubectl get po \
@@ -54,7 +62,9 @@ istioctl create-remote-secret \
   --name=cluster2 >cluster2-secret.yaml
 kubectl apply -f cluster2-secret.yaml --context="${CTX_CLUSTER1}"
 
-# kubectl label --context="${CTX_CLUSTER2}" namespace istio-system topology.istio.io/network=network1
+./gen-eastwest-gateway.sh \
+  --network network2 |
+  istioctl --context="${CTX_CLUSTER2}" install -y -f -
 
 ### VERIFY
 
@@ -78,12 +88,12 @@ kubectl apply --context="${CTX_CLUSTER1}" \
   -f helloworld.yaml \
   -l version=v1 -n sample
 
-# kubectl apply --context="${CTX_CLUSTER2}" \
-#   -f helloworld.yaml \
-#   -l version=v2 -n sample
+kubectl apply --context="${CTX_CLUSTER2}" \
+  -f helloworld.yaml \
+  -l version=v2 -n sample
 
 # curl https://raw.githubusercontent.com/istio/istio/release-1.27/samples/curl/curl.yaml -O
-# kubectl apply --context="${CTX_CLUSTER1}" \
-#   -f curl.yaml -n sample
+kubectl apply --context="${CTX_CLUSTER1}" \
+  -f curl.yaml -n sample
 kubectl apply --context="${CTX_CLUSTER2}" \
   -f curl.yaml -n sample
